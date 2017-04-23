65c7018e0692c388506eff4cde811ebfd8f4a3f5
Ubuntu <alidapopescu@gmail.com>
2017-04-23 17:45:25 +0000

    delete my solutions

--- my_solutions/thehardway/ex/ex11.py
+++
@@ -1,9 +0,0 @@
-print ("How old are you?")
-age =  input()
-print ("How tall are you?")
-height = input()
-print ("How fat are you?")
-weight = input()
-
-print ("So, you're %r old, %r tall and %r heavy." %
-        (age, height, weight))
--- my_solutions/thehardway/ex/ex12.py
+++
@@ -1,6 +0,0 @@
-age = input("How old are you? ")
-height = input ("How tall are you? ")
-weight =  input ("How fat are you? ")
-
-print ("So, you're %r old, %r tall and %r heavy. " %
-        (age, height, weight))
